e7d5e06de5b146aa4b44c310b695e5243c53b8c7
Aaron Lord <hire@aaron.codes>
2015-10-12 02:05:16 +0100

    Type stuff

---
+++ grunt/uglify.js
@@ -0,0 +1,10 @@
+module.exports = {
+    require: {
+        files: {
+            "js/plugins.min.js": [
+                "bower_components/jquery/dist/jquery.js",
+                "bower_components/jquery.typer/src/jquery.typer.js"
+            ]
+        }
+    }
+};
